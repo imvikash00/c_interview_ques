e0b6cb143a6777189feec0f29cec28a4b6e66cb0
root <root@freakode-Inspiron-N5110.(none)>
2014-06-13 15:36:26 +0530

    added recursive binary search fun

--- Find k closest elements to a given value.cpp
+++ Find k closest elements to a given value.cpp
@@ -16,11 +16,28 @@ int bsearch(int a[], int key, int low, int high)
 	}
 }
 
+int bRsearch(int a[], int key, int low, int high)
+{
+	int mid = (low+high)/2;
+	if(low<=high)
+	{
+		if(a[mid]==key)
+			return mid;
+		else if(a[mid]>key)
+			return bRsearch(a,key,low,mid-1);
+		else
+			return bRsearch(a,key,mid+1,high);
+	}
+	else
+		return -1;
+}
+
+
 int main()
 {
 
 	int a[10]={1,2,3,4,5,6,7,8,9,10};
-	cout<<bsearch(a,10,0,9);
+	cout<<bRsearch(a,10,0,9);
 	return 0;
 
 }
